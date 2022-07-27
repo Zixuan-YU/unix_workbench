8176e9b2082b8efca8692f328f6540c97ff4e599
Zixuan-YU <77198983+Zixuan-YU@users.noreply.github.com>
2022-07-27 17:00:07 -0400

    Add files via upload

--- condexif.sh
+++ condexif.sh
@@ -3,10 +3,10 @@
 
 if [[ $1 -gt 3 ]] && [[ $1 -lt 7 ]]
 then
-	echo "$1 is between 3 and 7"
+  echo "$1 is between 3 and 7"
 elif [[ $1 =~ "Jeff" ]] || [[ $1 =~ "Roger" ]] || [[ $1 =~ "Brian" ]]
 then
-	echo "$1 works in the Data Science Lab"
+  echo "$1 works in the Data Science Lab"
 else
-	echo "You entered: $1, not what I was looking for."
+  echo "You entered: $1, not what I was looking for."
 fi
